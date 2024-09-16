f2f380b67677026b8f7350097d6e0a450a02b979
khoadm3 <khoadmtlu15@gmail.com>
2024-09-16 22:41:42 +0700

    update code k8s

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae884866be8724b5db7abaebe69036a3-168577609.us-east-1.elb.amazonaws.com/:8080/api/v0'
 };
 
 /*
